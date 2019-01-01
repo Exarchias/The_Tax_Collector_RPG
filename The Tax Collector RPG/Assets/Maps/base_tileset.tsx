fc85b0ccd228e844f46e7e4f9197003611667c7c
Robert Kristian Alm <exarchiasghost@gmail.com>
2019-01-01 22:56:03 +0100

    Map
    Now my game has a map.
    But I am far from done. I already made some mistakes that I need to fix and it seems that it is much learning to do before I can say that I mastered the are of making maps.

---
+++ The Tax Collector RPG/Assets/Maps/base_tileset.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="base_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../../../../../Materials for Games/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="dirt" tile="578"/>
+  <terrain name="water" tile="60"/>
+ </terraintypes>
+ <tile id="2" terrain=",,,1"/>
+ <tile id="3" terrain=",,1,1"/>
+ <tile id="4" terrain=",,1,"/>
+ <tile id="59" terrain=",1,,1"/>
+ <tile id="61" terrain="1,,1,"/>
+ <tile id="116" terrain=",1,,"/>
+ <tile id="117" terrain="1,1,,"/>
+ <tile id="118" terrain="1,,,"/>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,0,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+</tileset>
